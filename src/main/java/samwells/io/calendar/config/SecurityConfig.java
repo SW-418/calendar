019a6dbf867d4922fb6d76697cfa0f72b8c8cb08
Sam Wells <samwells95@gmail.com>
2025-09-26 10:55:20 -0700

    Add swagger

--- src/main/java/samwells/io/calendar/config/SecurityConfig.java
+++ src/main/java/samwells/io/calendar/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,10 +19,21 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UserService userService) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        httpSecurity.sessionManagement(AbstractHttpConfigurer::disable);
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        httpSecurity.anonymous(Customizer.withDefaults());
 
         httpSecurity.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/api/v1/auth/signup").permitAll();
+            auth.requestMatchers(
+                    "/api/v1/auth/signup",
+                    "/swagger-ui.html",
+                    "/swagger-ui/index.html",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/v3/api-docs.yaml",
+                    "/swagger-resources/**",
+                    "/webjars/**"
+            ).permitAll();
             auth.anyRequest().authenticated();
         });
 
